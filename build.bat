513835cebc4f971cbce71e31db1b49cf733448b4
22-2 <17890m@protonmail.ch>
2026-04-02 21:27:55 +0900

    chore: ウイルス扱いされるのでビルドオプションを変更

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 uv run --with nuitka --with requests==2.31.0 --with beautifulsoup4==4.12.2 --with PyQt5==5.15.9 --with zstandard==0.23.0 ^
     python -m nuitka .\main.py ^
-    --onefile --standalone ^
+    --standalone ^
     --enable-plugin=pyqt5 ^
     --nofollow-import-to=tkinter ^
     --windows-disable-console ^
